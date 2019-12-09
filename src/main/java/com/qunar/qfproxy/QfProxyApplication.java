a6114fedc70937d68956bdfbc8d48ded01f12540
zhangchao11 <zhangchao11@360.cn>
2019-12-09 18:02:32 +0800

    modify qfproxy to spring boot

---
+++ src/main/java/com/qunar/qfproxy/QfProxyApplication.java
@@ -0,0 +1,11 @@
+package com.qunar.qfproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QfProxyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QfProxyApplication.class, args);
+    }
+}
